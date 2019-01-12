be24d58186674bde96500addfc31e89a0002b7a0
Rebwar Hosain Poori <Rebwar.poori@hotmail.com>
2019-01-12 12:36:39 +0330

    Add project files.

---
+++ SimpleDic/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SimpleDic.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
